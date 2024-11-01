8b129de06fab602844459f1ded2dc760f11587c3
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2024-11-01 18:31:05 -0300

    creating java swing
    creating application ui

---
+++ Dijstra_Algorithm/src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame windowFrame = new JFrame();
+        Window window = new Window();
+        window.WindowConfiguration(windowFrame);
+    }
+}
---
+++ Dijstra_Algorithm/src/Window.java
@@ -0,0 +1,24 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Window extends JPanel {
+
+    public void WindowConfiguration(JFrame windowFrame) {
+
+        JLabel mapa_title = new JLabel();
+        ImageIcon mapa_Brasil = new ImageIcon("C:\\Users\\mateus\\ws-java\\Dijkstra-Algorithm\\Dijkstra-Algorithm\\Dijstra_Algorithm\\utils\\image\\mapaBrasil.jpg");
+        mapa_title.setText("ESTADOS DO BRASIL");
+        windowFrame.add(mapa_title);
+
+        windowFrame.add(new JLabel(mapa_Brasil));
+        windowFrame.setVisible(true);
+        windowFrame.setSize(800, 800);
+        windowFrame.setResizable(false);
+        windowFrame.setTitle("PathFinder");
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
